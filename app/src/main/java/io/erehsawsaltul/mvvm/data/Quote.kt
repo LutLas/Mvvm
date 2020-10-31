65fcd8238f3166ee7f9f849ebdeb376accea9d5c
Muyoba Lutlas <lutakachihota@gmail.com>
2020-10-31 20:20:30 +0200

    2ndCommit

---
+++ app/src/main/java/io/erehsawsaltul/mvvm/data/Quote.kt
@@ -0,0 +1,5 @@
+package io.erehsawsaltul.mvvm.data
+
+class Quote {
+
+}
--- app/src/main/java/io/erehsawsaltul/mvvm/ui/quotes/QuotesActivity.kt
+++ app/src/main/java/io/erehsawsaltul/mvvm/ui/quotes/QuotesActivity.kt
@@ -1,11 +1,12 @@
-package io.erehsawsaltul.mvvm
+package io.erehsawsaltul.mvvm.ui.quotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.erehsawsaltul.mvvm.R
 
-class MainActivity : AppCompatActivity() {
+class QuotesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_quote)
     }
 }
